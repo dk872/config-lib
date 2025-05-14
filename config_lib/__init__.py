7e0cdba169312a9285731fe85003897f067ab72b
dk872 <dimakulyk2005@gmail.com>
2025-05-14 16:30:33 +0300

    Improve error handling during parsing and validation

--- config_lib/__init__.py
+++ config_lib/__init__.py
@@ -8,12 +8,21 @@ class ConfigManager:
     def __init__(self, file_path, schema_from_user=None):
         self.schema = schema_from_user or ConfigSchema().get_schema()
         self.file_path = file_path
-        self.config = load_config(file_path)
+        try:
+            self.config = load_config(file_path)
+        except Exception as e:
+            raise RuntimeError(f"Parse error from {file_path}: {e}")
 
     def validate(self):
-        config_validator = ConfigValidator(self.schema)
-        config_validator .validate(self.config)
-        print("Configuration is valid!")
+        if self.config is None:
+            raise ValueError("Loaded configuration is None. Check the config file.")
+
+        try:
+            config_validator = ConfigValidator(self.schema)
+            config_validator.validate(self.config)
+            print("Configuration is valid!")
+        except Exception as e:
+            raise ValueError(f"Validation error from {self.file_path}: {e}")
 
     def get_config(self):
         return self.config
